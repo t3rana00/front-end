23ac8c1e568dabf6938e1b3af7e141921fce970a
Rathnayake Pathiranage Nadeesha Subhashini Dilhani <143485986+t3rana00@users.noreply.github.com>
2024-03-24 17:46:16 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
-const BACKEND_ROOT_URL = 'http://localhost:3001'
+//const BACKEND_ROOT_URL = 'http://localhost:3001'
+const BACKEND_ROOT_URL = 'https://servertodo-klzq.onrender.com'
 import { Todos } from "./class/Todos.js";
 
 const todos = new Todos(BACKEND_ROOT_URL)
